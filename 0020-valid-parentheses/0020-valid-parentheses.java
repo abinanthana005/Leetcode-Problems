e3e14a0ec7d0be4fe5ace2f30e97a8336c3e9b02
Abinanthana M <abinanathanamahesh@gmail.com>
2025-09-14 19:08:50 +0530

    Time: 2 ms (96.79%), Space: 42.2 MB (16.59%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,21 @@
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c); // push opening bracket
+            } else {
+                if (stack.isEmpty()) return false; // no matching opening bracket
+                char top = stack.pop();
+
+                if (c == ')' && top != '(') return false;
+                if (c == '}' && top != '{') return false;
+                if (c == ']' && top != '[') return false;
+            }
+        }
+
+        return stack.isEmpty(); // all opened brackets closed properly
+    }
+}
